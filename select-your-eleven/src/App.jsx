00eed6fb20db3fd331a19b9f2b65218d8f5fc985
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-03-24 23:12:19 +0600

    Creat a navbar and components for banner  players

--- select-your-eleven/src/App.jsx
+++ select-your-eleven/src/App.jsx
@@ -2,14 +2,24 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
+import { Suspense } from 'react';
 import './App.css'
-
+import Banner from './Components/Navbar/Banner/Banner'
+import Navbar from './Components/Navbar/Navbar'
+import Players from './Components/Navbar/Players/Players';
+const fetchPlayers=async()=>{
+  const res=await fetch('/data.json');
+  return res.json();
+}
 function App() {
-
+const playersPromise=fetchPlayers()
 
   return (
     <>
-     
+     <Navbar></Navbar>
+     <Banner></Banner>
+<Suspense fallback={<span className="loading loading-spinner loading-lg"></span>
+}>     <Players playersPromise={playersPromise}></Players></Suspense>
     </>
   )
 }
---
+++ select-your-eleven/src/Components/Navbar/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='min-h-[60vh] bg-linear-to-r from-purple-400 to-purple-200'>
+            banner
+        </div>
+    );
+};
+
+export default Banner;
